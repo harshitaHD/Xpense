62a7017bf697d41488777d0c10bc6adc3e766a31
harshitaHD <harshitadhurwe@gmail.com>
2024-03-05 10:46:17 +0530

    updated frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import "./style/App.css";
 
 const App = () => {
   return (
